fb7d9fbbc7048e76ada8f095d2d215387e9d14b2
Hatwar Aman <121437677+ahatwar822@users.noreply.github.com>
2024-02-09 17:01:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
-// app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/', (req, res) => {
     res.render('login')
