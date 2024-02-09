4c9d822922f6b3591b89a6882df9a4f4dce7824f
Hatwar Aman <121437677+ahatwar822@users.noreply.github.com>
2024-02-09 17:02:14 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -4,12 +4,12 @@ mongoose.connect('mongodb://localhost:27017/athontication').then(() => {
     console.log("connected to database ; local mongodb server");
 }).catch((err) => {
     console.log(err);
-})
+});
 
 Schema = mongoose.Schema({
     uname: String,
     pass: String
-})
+});
 console.log("schema created");
 
 StudentModel = mongoose.model('student', Schema);
